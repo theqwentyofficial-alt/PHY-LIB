f0e30f5bc332c4c08e3bfe04739ac4e33455e288
few2alt <vasiline115@gmail.com>
2026-09-25 16:51:57 +0300

    feat: start developing Discord bot abstractions and core classes

---
+++ Bots/DiscordBots/D#+/BotData.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus;
+namespace PHY_LIB.Bots.DiscordBots.DSharpPlus
+{
+    public class BotData
+    {
+        public string Token { get; set; }
+        public DiscordClient client { get; set; }
+    }
+}
---
+++ Bots/DiscordBots/D#+/DataReader.cs
@@ -0,0 +1,15 @@
+﻿namespace PHY_LIB.Bots.DiscordBots.DSharpPlus
+{
+    public class DataReader
+    {
+        public async Task<string> ReadFile(string path)
+        {
+            using (StreamReader reader = new(path))
+            {
+                CancellationToken token = new();
+                string data = await reader.ReadToEndAsync(token);
+                return data;
+            }
+        }
+    }
+}
